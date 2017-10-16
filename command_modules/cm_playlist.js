5bdc025ac2f47f1aa0b7e4326fd6eb85ed0d46bb
Otard95 <stian.myklebostad@getmail.no>
2017-10-16 13:45:50 +0200

    playlist database check on init

--- command_modules/cm_playlist.js
+++ command_modules/cm_playlist.js
@@ -7,12 +7,27 @@ module.exports = function (commands, app) {
 
   console.log('Loading command module: Playlist...');
 
+  app.db.createDB('playlists').then((res) => {
+    console.log('Playlist database first time setup complete:\n'+res.message);
+  }).catch((err) => {
+    if (err.statusCode == app.db.codes.U_DATABASE_ALLREADY_EXISTS) {
+      console.log('Playlist database foud:\n'+err.humanreadable);
+    } else {
+      console.log('Failed to initialize database:\n'+err.message);
+      exit(2);
+    }
+  });
+
   let mod = {};
 
   mod.init = () => {
     commands.add('newPlaylist', (msg, params) => {
 
+      return new Promise((resolve, reject) => {
 
+
+
+      });
 
     }, 1, ['string'], 'newplaylist  -- Creates a new playlist with the name specified.\n' +
                       '             Example:\n' +
